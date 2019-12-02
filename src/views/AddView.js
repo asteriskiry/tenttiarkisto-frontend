910c31b7e498bc2394c886f1f791019f0f76515b
Arttu Huttunen <huttunen.arttu@gmail.com>
2019-12-02 19:51:17 +0200

    First draft of exam adding view

--- src/views/AddView.js
+++ src/views/AddView.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import MainComponent from '../components/main/MainComponent.js';
-import { Form } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import fi from 'date-fns/locale/fi';
+import axios from 'axios';
+
 //registerLocale('fi', fi)
 
 class AddView extends Component {
-  
-  state = {
-    startDate: null
+  constructor(propr) {
+    super(propr);
+
+    this.state = {
+      subjects: [],
+      courses: [],
+      startdate: null,
+    }
   }
+
+
+  async componentDidMount() {
+    try {
+      let subjects = await axios.get('exampleData/subjects.json');
+      let courses = await axios.get('exampleData/courses.json');
+      this.setState({
+        ...this.state,
+        ...{
+          subjects: subjects.data,
+          courses: courses.data,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
 
   handleChange = date => {
     this.setState({
@@ -19,6 +44,7 @@ class AddView extends Component {
   }
   
   render() {
+    const {courses} = this.state;
     return (
       <MainComponent>
         <h1>Lisää tentti</h1>
@@ -28,20 +54,37 @@ class AddView extends Component {
           <Form.Group controlId='SelectCourse'>
             <Form.Label>Valitse kurssi</Form.Label>
             <Form.Control as='select'>
-              <option>TRAK</option>
-              <option>Insinöörimatematiikka E :D</option>
+              {courses.map(course => (
+                <option>{course.name}</option>
+              ))}
             </Form.Control>
           </Form.Group>
           
           <Form.Group controlId='selectDate'>
-            <DatePicker
-                locale='fi'
-                selected={this.state.startDate}
-                onChange={this.handleChange}
-            />
+            <Form.Label>Tentin päivämäärä:</Form.Label>
+              <br />
+              <DatePicker
+                  locale='fi'
+                  selected={this.state.startDate}
+                  onChange={this.handleChange}
+              /> <br />
+            <Form.Label>Tentin tyyppi:</Form.Label>
+            <br />
+       
+            <Form.Check inline type='radio' label='Tentti' name='examTypeSelect' id='tentti' />
+            <Form.Check inline type='radio' label='Välikoe' name='examTypeSelect' id='valikoe' />
+            <br />
+            <label>Valitse tenttitiedosto: </label>            
+            <br />
+            <input type='file' />
 
           </Form.Group>
+
+          
         </Form>
+
+
+        <Button variant='outline-primary'>Lähetä tentti</Button>
 
       </MainComponent>
     );
